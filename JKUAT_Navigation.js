0f314f6ec743cd09c18bfa6f8a1fd4f6e08dab9c
Elias Baya <eliasbaya1223@gmail.com>
2023-02-23 15:04:31 +0300

    logic

--- JKUAT_Navigation.js
+++ JKUAT_Navigation.js
@@ -1,20 +1,43 @@
-// Use this sample to create your own voice commands
 intent('hello world', p => {
     p.play('(hello|hi there)');
 });
 
 
-// Give Alan some knowledge about the world
-corpus(`
-    Hello, I'm Alan.
-    This is a demo application.
-    You can learn how to teach Alan useful skills.
-    I can teach you how to write Alan Scripts.
-    I can help you. I can do a lot of things. I can answer questions. I can do tasks.
-    But they should be relevant to this application.
-    I can help with this application.
-    I'm Alan. I'm a virtual assistant. I'm here to help you with applications.
-    This is a demo script. It shows how to use Alan.
-    You can create dialogs and teach me.
-    For example: I can help navigate this application.
-`);
+intent('take me to $(PLACE* (.*))', p=>{
+    let place = project.places.filter(x=> x.name.toLowerCase() === p.PLACE.toLowerCase() || x.short_name.toLowerCase() === p.PLACE.toLowerCase())[0];
+    
+    try{
+        p.play({"command": "take_me_to", "id": place.id});
+        p.play("(Please wait | On it | Okay Boss | Fetching direction)");
+        
+    }catch(err){
+        console.log("I can't locate this place");
+        p.play("Sorry, I can't locate this place");
+    }
+});
+
+intent('i want to go to $(PLACE* (.*))', p=>{
+    let place = project.places.filter(x=> x.name.toLowerCase() === p.PLACE.toLowerCase() || x.short_name.toLowerCase() === p.PLACE.toLowerCase())[0];
+    
+    try{
+        p.play({"command": "take_me_to", "id": place.id});
+        p.play("(Please wait | On it | Okay Boss | Fetching direction)");
+        
+    }catch(err){
+        console.log("I can't locate this place");
+        p.play("Sorry, I can't locate this place");
+    }
+});
+
+intent('how do i get to $(PLACE* (.*))', p=>{
+    let place = project.places.filter(x=> x.name.toLowerCase() === p.PLACE.toLowerCase() || x.short_name.toLowerCase() === p.PLACE.toLowerCase())[0];
+    
+    try{
+        p.play({"command": "take_me_to", "id": place.id});
+        p.play("(Please wait | Preparing your journey | Calculating your direction | Fetching direction)");
+        
+    }catch(err){
+        console.log("I can't locate this place");
+        p.play("Sorry, I can't locate this place");
+    }
+});
