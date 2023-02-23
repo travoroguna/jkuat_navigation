9ac5a919303d169630f4770ef576037cf0cfb2d6
Elias Baya <46612232+ekbaya@users.noreply.github.com>
2023-02-23 13:46:18 +0300

    First Init

---
+++ JKUAT_Navigation.js
@@ -0,0 +1,20 @@
+// Use this sample to create your own voice commands
+intent('hello world', p => {
+    p.play('(hello|hi there)');
+});
+
+
+// Give Alan some knowledge about the world
+corpus(`
+    Hello, I'm Alan.
+    This is a demo application.
+    You can learn how to teach Alan useful skills.
+    I can teach you how to write Alan Scripts.
+    I can help you. I can do a lot of things. I can answer questions. I can do tasks.
+    But they should be relevant to this application.
+    I can help with this application.
+    I'm Alan. I'm a virtual assistant. I'm here to help you with applications.
+    This is a demo script. It shows how to use Alan.
+    You can create dialogs and teach me.
+    For example: I can help navigate this application.
+`);
